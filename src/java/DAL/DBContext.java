d2b2c2b8d0afa24fc208520dbfe3f32eb87be463
DoThanhdz <124509686+DoThanhdz@users.noreply.github.com>
2023-02-07 16:42:57 +0700

    add database,library

---
+++ src/java/DAL/DBContext.java
@@ -0,0 +1,34 @@
+package DAL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FPT University - PRJ301
+ */
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+
+        try {
+            String user = "sa";
+            String pass = "tT123412345";
+            String url = "jdbc:sqlserver://LAPTOP-7MBFLMH0:1433;databaseName=PRJ301_DB_Ver2";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
